b115339b5fdad45ff5b71392039ecef6651e4a06
Onionss-2811 <anhtu28112001@gmail.com>
2022-03-23 17:51:17 +0700

    T4 Thg3 23

---
+++ src/node1.erl
@@ -0,0 +1,48 @@
+%% @author tukna
+%% @doc @todo Add description to node1.
+
+
+-module(node1).
+-behaviour(gen_server).
+
+-export([start/0, send/3, m_receive/3]).
+-export([init/1, handle_call/3, handle_cast/2]).
+
+start() ->
+    Return = gen_server:start_link({global, node1}, node1, [], []),
+    io:format("start_node1: ~p~n", [Return]),
+    Return.
+
+send(M_from, M_to, Mess) -> 
+	gen_server:call({global,M_from}, {send,M_from, M_to, Mess}).
+
+m_receive(M_from, M_to, Mess) ->
+	gen_server:call({global,M_to}, {m_receive,M_from, M_to, Mess}).
+
+init([]) ->
+    State = [],
+    Return = {ok, State},
+    io:format("init: ~p~n", [State]),
+    Return.
+
+handle_call(get_status, _From, State) ->
+    Reply = get_status_ok,
+    {reply, Reply, State};
+
+handle_call({send,M_from, M_to, Mess}, _From, State) ->
+	io:format("You send to ~p: ~p ~n",[M_to, Mess]),
+	node2:m_receive(M_from, M_to, Mess),
+    {reply, ok, State};
+
+
+handle_call({m_receive,M_from, _M_to, Mess}, _From, State) ->
+    io:format("Message from ~p: ~p ~n",[M_from, Mess]),
+    {reply, ok, State}.
+
+
+
+handle_cast({send,Mess}, State) ->
+    io:format("Receive msg from handle_cast: ~p~n", [Mess]),
+    {noreply, State}.
+
+
